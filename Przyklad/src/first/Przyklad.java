cf25990d3be65e61ed244aed1d0eb28f3004f4ce
s18864 <s18864@lab4l-2.pjwstk.edu.pl>
2019-02-23 15:11:22 +0100

    Powód dla którego wrzucam te pliki

---
+++ Przyklad/src/first/Przyklad.java
@@ -0,0 +1,15 @@
+package first;
+
+public class Przyklad {
+	
+	public static void main(String[] args)
+	{
+		for (int i=0; i<1000; i++)
+		{
+			System.out.print ("Ala ma kota.\n");
+		}
+		
+	}
+	
+	
+}
