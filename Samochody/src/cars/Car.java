a9bb15e16f42e591f276937e0b234009bdb0ea2c
s18864 <s18864@lab4l-2.pjwstk.edu.pl>
2019-02-23 15:46:20 +0100

    pierwszeWprowadzenie

---
+++ Samochody/src/cars/Car.java
@@ -0,0 +1,11 @@
+package cars;
+
+public class Car {
+	
+	public String brand;
+	public boolean used;
+	public int mileage;
+	public double price;
+	
+	
+}
---
+++ Samochody/src/cars/Przyklad.java
@@ -0,0 +1,18 @@
+package cars;
+
+public class Przyklad {
+	
+	public static void main(String[] args)
+	{
+		Car car = new Car();
+		car.brand = "BMW";
+		car.mileage = 10000;
+		car.used = false;
+		car.price = 20000;
+		
+		
+		
+	}
+	
+	
+}
